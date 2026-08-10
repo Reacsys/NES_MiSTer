8f9d2504d4c68289aa8c38a87e6a5250991b6c54
Reacsys <704nd0@gmail.com>
2026-08-10 17:30:21 +0300

    Update NES.sv

--- NES.sv
+++ NES.sv
@@ -120,7 +120,7 @@ parameter CONF_STR = {
 	"P4-;",
 	"P4O[66:65],RAM Clear,No,$00,$FF,Random;",
 	"P4O[64],PPU Reset Behavior,Famicom,NES;",
-	"P4O[71:70],CPU Turbo,Off,VBlank 2x,VBlank 4x,Always 2x;",
+	"P4O[82:81],CPU Turbo,Off,VBlank 2x,VBlank 4x,Always 2x;",
 	"P4OQ,Video Dijitter,Enabled,Disabled;",
 	"P4O[69],Debug Dots,Off,On;",
 	"H8P5,Vs. DIPs;",
@@ -788,7 +788,7 @@ NES nes (
 	.apu_ce          (apu_ce),
 	// Video
 	.ex_sprites      (status[25]),
-	.turbo           (status[71:70]),
+	.turbo           (status[82:81]),
 	.color           (color),
 	.hsync           (nes_hsync),
 	.hblank          (nes_hblank),
